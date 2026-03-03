687d506a9be95d63803a37a6525ccb10f7a8fa95
nokeCode <nokegermain@gmail.com>
2026-03-03 16:23:22 +0000

    correction de settings

--- HStore/settings.py
+++ HStore/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =====================================================
 # SECURITY
 # =====================================================
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY ='django-insecure-r8lo%v+^sddfe+q8nh3kqal&_%-c6k^4d_um43ujdip$#qz_#b'
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['*']
 
